6298128378652596dcc009e6c9a5df32edc037b2
ThibautSF <tsimonfine@gmail.com>
2019-10-01 19:33:01 +0200

    Initial Commit (final)

---
+++ Assets/Scripts/TransformFunctions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFunctions : MonoBehaviour
+{
+	public float moveSpeed = 2f;
+    public float moveRunSpeed = 5f;
+	public float turnSpeed = 100f;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float mSpeed = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? moveRunSpeed : moveSpeed;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z)) {
+        	transform.Translate(Vector3.forward * mSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+        	transform.Translate(-Vector3.forward * mSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q)) {
+        	transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+        	transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+    }
+}
