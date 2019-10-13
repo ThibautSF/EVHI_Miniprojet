85586c0981df1d1ee237bd29b80b0b3ffba2f3de
ThibautSF <tsimonfine@gmail.com>
2019-10-13 20:41:33 +0200

    Room 1 and 2
    - Weapon system
    - Patrol system

--- MyLittleGame/Assets/Scripts/TransformFunctions.cs
+++ MyLittleGame/Assets/Scripts/TransformFunctions.cs
@@ -2,34 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformFunctions : MonoBehaviour
-{
+public class TransformFunctions : MonoBehaviour {
 	public float moveSpeed = 2f;
-    public float moveRunSpeed = 5f;
+	public float moveRunSpeed = 5f;
 	public float turnSpeed = 100f;
 
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
+	// Start is called before the first frame update
+	void Start() {
+		
+	}
 
-    // Update is called once per frame
-    void Update() {
-        float mSpeed = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? moveRunSpeed : moveSpeed;
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Z)) {
-        	transform.Translate(Vector3.forward * mSpeed * Time.deltaTime);
-        }
+	// Update is called once per frame
+	void Update() {
+		float mSpeed = (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) ? moveRunSpeed : moveSpeed;
+		
+		float movement = Input.GetAxis("Vertical");
+		movement *= Time.deltaTime * mSpeed;
+		float sideStep = Input.GetAxis ("Horizontal");
+		sideStep *= Time.deltaTime * mSpeed;
 
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
-        	transform.Translate(-Vector3.forward * mSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Q)) {
-        	transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-        	transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-        }
-    }
+		this.transform.Translate(Vector3.forward * movement);
+		this.transform.Translate(Vector3.right * sideStep);
+	}
 }
